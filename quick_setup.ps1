98b7ee7bcf90455684e85db00d3f4485e331aec7
Andrey Garmash <angarmas@microsoft.com>
2023-10-26 09:29:53 +0200

    Update quick_setup.ps1 to use  ml extension (v2)

--- quick_setup.ps1
+++ quick_setup.ps1
@@ -2,12 +2,13 @@
 .DESCRIPTION
     This script has the following dependencies:
        * a pre-installed Azure CLI (not the PowerShell one).
-       * a pre-installed ML extension azure-cli-ml.
+       * a pre-installed ML extension ml (v2).
     
     Creates Azure resources required by demand forecast logic for FinOps.
     * Creates a resource group.
     * Creates a storage account
     * Creates and inits an ML workspace:
+        Creates a datastore for the workspace.
         Creates a compute instance for script run.
         Creates a compute cluster for pipeline run.
     * Creates a service principal used to access created resources.
@@ -41,10 +42,11 @@ param (
 
 $storageContainer = "demplan-azureml"  #should not be changed
 $computeCluster_Name = "e2ecpucluster" #should not be changed
+$workspaceBlobDS = "workspaceblobdemplan"
 
 function Set-SubscriptionContext{
-	# check the subscription
-	$res = az account list --query "[?id=='$subscriptionId'].id" --output tsv
+    # check the subscription
+    $res = az account list --query "[?id=='$subscriptionId'].id" --output tsv
     if ([string]::IsNullOrEmpty($res)) {
         Write-Warning "Subscription $subscriptionId is not valid for the current account."
         throw
@@ -73,7 +75,7 @@ function Check-InputParams {
         throw
     }
     
-     $res = az ad app list --display-name $AADApplicationName --query "[0].appId" --output tsv
+    $res = az ad app list --display-name $AADApplicationName --query "[0].appId" --output tsv
     if ($res) {
         Write-Warning "Azure application $AADApplicationName already exists and cannot be created."
         throw
@@ -106,26 +108,27 @@ function Create-StorageAccount() {
 
     # create a storage account
     Write-Host "Creating a storage account $storageAccountName in $location ..."
-    $res = az storage account create --name $storageAccountName --resource-group $resourceGroupName --location $location --sku $storageSKU --access-tier $storageAccessTier
+    $responseObj = az storage account create --name $storageAccountName --resource-group $resourceGroupName --location $location --sku $storageSKU --access-tier $storageAccessTier `
+        | ConvertFrom-Json
 
     if ($LASTEXITCODE -ne 0) {
         # failed creating ML storage account
         Write-Warning "Error while trying to create a storage account."
-        throw $res
+        throw $responseObj
     }
 
-    $script:storageAccountId = az storage account show --resource-group $resourceGroupName --name $storageAccountName --query "id" --output tsv
+    $script:storageAccountId = $responseObj.id
     $script:storageAccessKey = az storage account keys list --resource-group $resourceGroupName --account-name $storageAccountName --query "[0].value" --output tsv
 
     # create a BLOB container used by forecast logic
     $res = az storage container create --name $storageContainer --account-name $storageAccountName --resource-group $resourceGroupName --account-key $storageAccessKey
-		if ($LASTEXITCODE -ne 0) {
-		# failed creating BLOB container
-		Write-Warning "Error while trying to create a BLOB container."
-		throw $res
+    if ($LASTEXITCODE -ne 0) {
+        # failed creating BLOB container
+        Write-Warning "Error while trying to create a BLOB container."
+        throw $res
     }
 
-	# create a lifetime policy to auto delete blob in the forecast container
+    # create a lifetime policy to auto delete blob in the forecast container
     $rule = @{ definition = @{actions = @{baseBlob = @{delete = @{daysAfterModificationGreaterThan = $BlobDeleteAfterDaysNumber}}}; filters = @{blobTypes = ,"blockBlob"; prefixMatch = ,$storageContainer}}; type = "Lifecycle"; name = $ruleName; enabled = $true;}
     $jsonPolicy = @{ rules = ,$rule; } | ConvertTo-json -Depth 10 -Compress
     $jsonPolicy = $jsonPolicy -replace '"', '\"'
@@ -138,51 +141,76 @@ function Create-StorageAccount() {
     }
 }
 
-function Create-Workspace() {
-    $WorkSpaceBlobDefaultDS = "workspaceblobdemplan"
+function Create-DataStoreConfigFile() {
+    $filePath = ".\wsBlobDSDefinition.yml"
 
+    $res = New-Item -Path $filePath -ItemType File
+
+    $configData = 
+@"
+        `$schema: https://azuremlschemas.azureedge.net/latest/azureBlob.schema.json
+        name: $workspaceBlobDS
+        type: azure_blob
+        description: Datastore with blob container used by demand planning ML.
+        account_name: $storageAccountName
+        container_name: $storageContainer
+        credentials:
+            account_key: $storageAccessKey
+"@
+
+    #write the config data
+    Set-Content $filePath $configData
+    return $filePath
+}
+
+function Create-Workspace() {
     # create ML workspace
     Write-Host "Creating an ML workspace $workspaceName ..."
-    $res = az ml workspace create --workspace-name $workspaceName --resource-group $resourceGroupName --storage-account $storageAccountId
+    $responseObj = az ml workspace create --name $workspaceName --resource-group $resourceGroupName --storage-account $storageAccountId `
+        | ConvertFrom-Json
 
     if ($LASTEXITCODE -ne 0) {
         # failed creating ML workspace
         Write-Warning "Error while trying to create an ML workspace."
-        throw $res
+        throw $responseObj
     }
 
-    $script:workspaceId = az ml workspace show --resource-group $resourceGroupName --workspace-name $workspaceName --query "id" --output tsv
+    $script:workspaceId = $responseObj.id
 
-    Write-Host "Setting up an ML workspace datastore ..."
-    $res = az ml datastore attach-blob --account-name $storageAccountName --container-name $storageContainer --name $WorkSpaceBlobDefaultDS `
-        --account-key $storageAccessKey --workspace-name $workspaceName --resource-group $resourceGroupName `
-        --storage-account-resource-group $resourceGroupName --storage-account-subscription-id $subscriptionId
+    $dsConfigurationFileName = Create-DataStoreConfigFile 
 
-    if ($LASTEXITCODE -ne 0) {
-        # failed attaching datastore
-        Write-Warning "Error while trying to attach an ML workspace datastore."
-        throw $res
+    try {
+        Write-Host "Setting up an ML workspace datastore ..."
+        $res = az ml datastore create --file $dsConfigurationFileName --resource-group $resourceGroupName --workspace-name $workspaceName --subscription $subscriptionId
+
+        if ($LASTEXITCODE -ne 0) {
+            # failed attaching datastore
+            Write-Warning "Error while trying to create an ML workspace datastore."
+            throw $res
+        }
     }
-
-    $res = az ml datastore set-default --name $WorkSpaceBlobDefaultDS --resource-group $resourceGroupName --workspace-name $WorkspaceName
-
-    if ($LASTEXITCODE -ne 0) {
-        # failed setting default datastore
-        Write-Warning "Error while trying to set up a default ML datastore."
-        throw $res
+    finally {
+        if  ($dsConfigurationFileName -And (Test-Path $dsConfigurationFileName)) {
+            Remove-Item $dsConfigurationFileName
+        }
     }
-
-	# make sure access keys are in sync
-    az ml workspace sync-keys  --workspace-name $WorkspaceName --resource-group $resourceGroupName
 }
 
 function Create-ComputeInstance() {
-    $computeInstance_Name = "notebookScryptExecutor"
     $computeInstance_vm_size = "Standard_D3_v2"
+    $defaultIdleTimeForShutdownInMin = 60
+
+    $date = Get-Date
+    $timePortion = $date.ToString("MMddhhmmss")  # 10 chars
+    $baseInstance_Name = "scriptExecutor"        # 14 chars
+
+    #as of now the name length is limited with 24 characters
+    $uniqueInstanceName = $baseInstance_Name + $timePortion
 
     # create workspace compute instance
     Write-Host "Creating an ML workspace compute instance ..."
-    $res = az ml computetarget create computeinstance --name $computeInstance_Name --resource-group $resourceGroupName --workspace-name $workspaceName --vm-size $computeInstance_vm_size
+    $res = az ml compute create --type computeinstance --name $uniqueInstanceName --resource-group $resourceGroupName `
+        --workspace-name $workspaceName --size $computeInstance_vm_size --set "idle_time_before_shutdown_minutes=$defaultIdleTimeForShutdownInMin"
 
     if ($LASTEXITCODE -ne 0) {
         # failed creating workspace compute instance
@@ -198,8 +226,8 @@ function Create-ComputeCluster() {
 
     # create workspace compute cluster
     Write-Host "Creating an ML workspace compute cluster ..."
-    $res = az ml computetarget create amlcompute --name $computeCluster_Name --resource-group $resourceGroupName --workspace-name $workspaceName `
-        --min-nodes $computeCluster_min_nodes --max-nodes $computeCluster_max_nodes --vm-size $computeCluster_vm_size
+    $res = az ml compute create --type amlcompute --name $computeCluster_Name --resource-group $resourceGroupName --workspace-name $workspaceName `
+        --min-instances $computeCluster_min_nodes --max-instances $computeCluster_max_nodes --size $computeCluster_vm_size
 
     if ($LASTEXITCODE -ne 0) {
         # failed creating compute cluster
@@ -221,15 +249,15 @@ function Create-RoleForScope([string]$assignee, [string] $role, [string] $scope)
 function Create-AppWithPrincipal{
     # create AAD application for forecast access
     Write-Host "Creating an AAD application for forecast access ..."
-    $appResponse = az ad app create --display-name $AADApplicationName | ConvertFrom-Json
+    $responseObj = az ad app create --display-name $AADApplicationName | ConvertFrom-Json
 
     if ($LASTEXITCODE -ne 0) {
         # failed creating AAD application
         Write-Warning "Error while trying to create an AAD application."
-        throw $appResponse
+        throw $responseObj
     }
 
-    $script:appId = $appResponse.appId
+    $script:appId = $responseObj.appId
 
     # create service principal
     Write-Host "Creating a service principal for an application $AADApplicationName ..."
@@ -249,12 +277,12 @@ function Create-AppWithPrincipal{
 
 function Display-ScriptResult{
     $azureTenantId = az account show --query "tenantId" --output tsv
-	$documentationLink = "https://go.microsoft.com/fwlink/?linkid=2165514"
+    $documentationLink = "https://go.microsoft.com/fwlink/?linkid=2165514"
 
-	Write-Host "`nDemand forecast setup script has completed."
-	Write-Host "Please create an application secret explicitly and proceed with workspace pipeline configuration according to public documentation: $documentationLink"
+    Write-Host "`nDemand forecast setup script has completed."
+    Write-Host "Please create an application secret explicitly and proceed with workspace pipeline configuration according to public documentation: $documentationLink"
     
-	# display information needed by FinOps for AML demand forecast	
+    # display information needed by FinOps for AML demand forecast
     Write-Host "`nDemand forecast parameters.`n"
     Write-Host "Azure tenant id: $azureTenantId"
     Write-Host "Storage account name: $storageAccountName"
@@ -281,19 +309,31 @@ function Do-ResourcesCleanUp {
     }
 }
 
+function Check-AzureClientVersion {
+    $isAzInstalled = Get-Command az -ErrorAction SilentlyContinue
+    if (-not $isAzInstalled) {
+        throw "AZ CLI is not installed, please go get it at: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli"
+    }
+
+    $urlToSetupMLExtensionV2 = "https://learn.microsoft.com/en-us/azure/machine-learning/how-to-configure-cli?view=azureml-api-2"
+
+    $mlExtensionV1 =  az extension list --query "[?name=='azure-cli-ml'].name" --output tsv
+    if ($mlExtensionV1) {
+        throw "Outdated version of ml extension (v1) for AZ CLI is installed. Please deinstall it and install ml extension (v2), see: $urlToSetupMLExtensionV2"	
+    }
+
+    $mlExtensionV2 =  az extension list --query "[?name=='ml'].name" --output tsv
+    if (!$mlExtensionV2) {
+        throw "ML extension (v2) for AZ CLI is not installed. Please install it, see: $urlToSetupMLExtensionV2"
+    }
+}
+
 #====
 
 $ErrorActionPreference = "Stop"
 
-$isAzInstalled = Get-Command az -ErrorAction SilentlyContinue
-if (-not $isAzInstalled) {
-    throw "AZ CLI is not installed, please go get it at: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli"
-}
-
-$mlExtensionName =  az extension list --query "[?name=='azure-cli-ml'].name" --output tsv
-if ([string]::IsNullOrEmpty($mlExtensionName)){
-    throw "ML extension for AZ CLI azure-cli-ml is not installed. Please install it, see: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli"
-}
+#check azure client and extension versions
+Check-AzureClientVersion
 
 # login to azure
 Write-Host "Please log in with your Azure credentials..."
